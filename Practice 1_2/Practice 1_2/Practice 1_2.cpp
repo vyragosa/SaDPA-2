d455c0f7279ef5dee8d6fe745dfa9d8c9ba6e928
sad <39193849+vyragosa@users.noreply.github.com>
2022-09-04 20:24:52 +0300

    Finished

--- Practice 1_2/Practice 1_2/Practice 1_2.cpp
+++ Practice 1_2/Practice 1_2/Practice 1_2.cpp
@@ -1,23 +1,25 @@
 ﻿#include <iostream>
-#define size (10000000 / sizeof(uint32_t) + 1)
 #define byteUInt (sizeof(uint32_t) * 8)
+#define size (9000000 / byteUInt + 1)
 
-void binaryOutput(uint32_t x, uint32_t pos)
-{
+void binaryOutput(uint32_t num, uint32_t pos) {
 	uint32_t  mask = 1 << (byteUInt - 1);
 	for (uint32_t i = 1; i <= byteUInt; i++) {
-		if ((x & mask) >> (byteUInt - i + i) == 1)
-			std::cout << i + pos << '\t';
+		if ((num & mask) >> (byteUInt - i) == 1)
+			std::cout << i + pos - 1 + 1000000 << '\t';
 		mask >>= 1;
 	}
 }
+
 void sorting(uint32_t* arr, uint32_t arr_size) {
 	int num;
 	for (int i = 0; i < arr_size; i++) {
 		std::cin >> num;
-		arr[num / byteUInt] |= 1 << (byteUInt - (num % 32));
+		num -= 1000000;
+		arr[num / byteUInt] |= 1 << (byteUInt - (num % byteUInt) - 1);
 	}
 }
+
 void output(uint32_t* arr, uint32_t arr_size) {
 	uint32_t num;
 	for (int i = 0; i < size; i++) {
@@ -25,12 +27,10 @@ void output(uint32_t* arr, uint32_t arr_size) {
 	}
 }
 
-
-int main()
-{
+int main() {
 	uint32_t arr_size;
 	std::cin >> arr_size;
-	uint32_t* arr = new uint32_t[size] {0};
+	uint32_t* arr = new uint32_t[size]{ 0 };
 	sorting(arr, arr_size);
 	output(arr, arr_size);
 	return 0;
