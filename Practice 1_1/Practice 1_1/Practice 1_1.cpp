60ada2cb0a9d4e3581a0758dd0b5f38e7d781b24
sad <39193849+vyragosa@users.noreply.github.com>
2022-09-04 17:44:07 +0300

    Added first practice

---
+++ Practice 1_1/Practice 1_1/Practice 1_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#define byteInt sizeof(uint32_t) * 8
+int firstOperation(uint32_t num) {
+	return num | (1 << (byteInt - 5) | 1 << (byteInt - 7) | 1 << (byteInt - 13));
+}
+int secondOperation(uint32_t num) {
+	return num & ~ (1 << (byteInt - 1) | 1 << (byteInt - 2) | 1 << (byteInt - 3) | 1 << (byteInt - 4));
+}
+int thirdOperation(uint32_t num) {
+	return num << 9;
+}
+
+int fourthOperation(uint32_t num) {
+	return num >> 7;
+}
+int fifthOperation(uint32_t num, uint32_t bitToChange) {
+	uint32_t mask = 1 << (byteInt - 1);
+	return num & ~(mask >> (bitToChange - 1));
+}
+
+int main() {
+	uint32_t num = 0xfa, bitToChange;
+	std::cout << "1. result: " << std::hex << std::uppercase << firstOperation(num) << std::endl;
+	std::cout << "Enter number: ";
+	std::cin >> num;
+	std::cout << "Your number: " << num << std::endl;
+	std::cout << "2. result: " << secondOperation(num) << std::endl;
+	std::cout << "Enter number: ";
+	std::cin >> num;
+	std::cout << "3. result: " << std::dec << thirdOperation(num) << std::endl;
+	std::cout << "Enter number: ";
+	std::cin >> num;
+	std::cout << "4. result: " << std::dec << fourthOperation(num) << std::endl;
+	std::cout << "Enter number and bit u want to change: ";
+	std::cin >> num >> bitToChange;
+	std::cout << "Your number: " << std::hex << num << std::endl;
+	std::cout << "5. result: " << fifthOperation(num, bitToChange) << std::endl;
+	return 0;
+}
