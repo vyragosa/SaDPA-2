0ddf1a4d08de03648fe1e10f460c90e44584e5bc
sad <39193849+vyragosa@users.noreply.github.com>
2022-10-01 19:13:53 +0300

    formatting issue

--- Classwork2/main.cpp
+++ Classwork2/main.cpp
@@ -2,94 +2,93 @@
 #include <fstream>
 
 struct book {
-    int isbn;
-    char namebook[20];
-    char author[30];
-    unsigned int year_of_publication;
+	int isbn;
+	char namebook[20];
+	char author[30];
+	unsigned int year_of_publication;
 };
 
 int createBinFile(std::string nametf, std::string namebf) {
-    std::ifstream itf(nametf);
-    if (!itf)
-        return -1;
-    book b;
-    std::fstream obf(namebf, std::ios::binary | std::ios::out);
-    while (!itf.eof()) {
-        itf >> b.isbn;
-        itf.get();
-        itf.getline(b.namebook, 30, '\n');
-        itf.getline(b.author, 30, '\n');
-        itf >> b.year_of_publication;
-        obf.write((char*)&b, sizeof(book));
-    }
-    itf.close();
-    obf.close();
-    return 0;
+	std::ifstream itf(nametf);
+	if (!itf)
+		return -1;
+	book b;
+	std::fstream obf(namebf, std::ios::binary | std::ios::out);
+	while (!itf.eof()) {
+		itf >> b.isbn;
+		itf.get();
+		itf.getline(b.namebook, 30, '\n');
+		itf.getline(b.author, 30, '\n');
+		itf >> b.year_of_publication;
+		obf.write((char *) &b, sizeof(book));
+	}
+	itf.close();
+	obf.close();
+	return 0;
 }
 
 int outBinFile(std::string namebf) {
-    std::fstream ibf(namebf, std::ios::binary | std::ios::in);
-    if (!ibf)
-        return 1;
-    book b;
-    int len = sizeof(book);
-    ibf.read((char*)&b, len);
-    while (!ibf.eof()) {
-        std::cout << b.isbn << ' ' << b.namebook
-                  << ' ' << b.author << ' ' << b.year_of_publication <<std::endl;
-        ibf.read((char*)&b, len);
+	std::fstream ibf(namebf, std::ios::binary | std::ios::in);
+	if (!ibf)
+		return 1;
+	book b;
+	int len = sizeof(book);
+	ibf.read((char *) &b, len);
+	while (!ibf.eof()) {
+		std::cout << b.isbn << ' ' << b.namebook
+				  << ' ' << b.author << ' ' << b.year_of_publication << std::endl;
+		ibf.read((char *) &b, len);
 
-    }
-    ibf.close();
-    return 0;
+	}
+	ibf.close();
+	return 0;
 }
 
 int searchKeyInBinFile(std::string namebf, int key) {
-    std::fstream ibf(namebf, std::ios::binary | std::ios::in);
-    if (!ibf)
-        return 1;
-    book b;
-    int i = 0;
-    int len = sizeof(book);
-    ibf.read((char*)&b, len);
-    while (!ibf.eof()) {
-        i++;
-        if (key == b.isbn) {
-            ibf.close();
-            return i;
-        }
+	std::fstream ibf(namebf, std::ios::binary | std::ios::in);
+	if (!ibf)
+		return 1;
+	book b;
+	int i = 0;
+	int len = sizeof(book);
+	ibf.read((char *) &b, len);
+	while (!ibf.eof()) {
+		i++;
+		if (key == b.isbn) {
+			ibf.close();
+			return i;
+		}
 
-        ibf.read((char*)&b, len);
+		ibf.read((char *) &b, len);
 
-    }
-    ibf.close();
-    return -1;
+	}
+	ibf.close();
+	return -1;
 }
 
-int directAccessToTheRecordBinFile(std::string namebf, int numRecord, book& r) {
-    int code;
-    std::fstream bf(namebf, std::ios::binary | std::ios::in);
-    if (!bf) {
-        return 2;
-    }
+int directAccessToTheRecordBinFile(std::string namebf, int numRecord, book &r) {
+	int code;
+	std::fstream bf(namebf, std::ios::binary | std::ios::in);
+	if (!bf) {
+		return 2;
+	}
 
-    int offset = (numRecord - 1) * sizeof(r);
-    bf.seekg(offset, std::ios::beg);
-    if (!bf.bad()) {
-        bf.read((char*)&r, sizeof(book));
-        code = 0;
-    }
-    else
-        code = 1;
-    bf.close();
-    return code;
+	int offset = (numRecord - 1) * sizeof(r);
+	bf.seekg(offset, std::ios::beg);
+	if (!bf.bad()) {
+		bf.read((char *) &r, sizeof(book));
+		code = 0;
+	} else
+		code = 1;
+	bf.close();
+	return code;
 }
 
 int main() {
-    createBinFile("TextFile1.txt", "BinFile.bin");
-    outBinFile("BinFile.bin");
-    std::cout << searchKeyInBinFile("BinFile.bin", 222) << std::endl;
-    book b;
-    directAccessToTheRecordBinFile("BinFile.bin", 3, b);
-    std::cout << b.isbn << std::endl;
+	createBinFile("TextFile1.txt", "BinFile.bin");
+	outBinFile("BinFile.bin");
+	std::cout << searchKeyInBinFile("BinFile.bin", 222) << std::endl;
+	book b;
+	directAccessToTheRecordBinFile("BinFile.bin", 3, b);
+	std::cout << b.isbn << std::endl;
 }
--- Practice1_2/main.cpp
+++ Practice1_2/main.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
+
 #define UINT_BIT (sizeof(uint32_t) * 8)
 #define UINT_SHIFT (sizeof(uint32_t) + 1)
 #define SIZE (8999999 / UINT_BIT + 1)
 
 
-void sorting(uint32_t* arr);
-void output(const uint32_t* arr);
+void sorting(uint32_t *arr);
+
+void output(const uint32_t *arr);
 
 int main() {
-    uint32_t* arr = new uint32_t[SIZE]{ 0 };
-    sorting(arr);
-    output(arr);
-    return 0;
+	uint32_t *arr = new uint32_t[SIZE]{0};
+	sorting(arr);
+	output(arr);
+	return 0;
 }
 
-void sorting(uint32_t* arr) {
-    uint32_t num, arr_size;
-    std::cout << "Enter size of array: ";
-    std::cin >> arr_size;
-    std::cout << "Enter array:\n";
-    for (int i = 0; i < arr_size; i++) {
-        std::cin >> num;
-        if (num > 9999999 || num < 1000000) {
-            std::cerr << "The number must contain only 7 digits!\n";
-            exit(-1);
-        }
+void sorting(uint32_t *arr) {
+	uint32_t num, arr_size;
+	std::cout << "Enter size of array: ";
+	std::cin >> arr_size;
+	std::cout << "Enter array:\n";
+	for (int i = 0; i < arr_size; i++) {
+		std::cin >> num;
+		if (num > 9999999 || num < 1000000) {
+			std::cerr << "The number must contain only 7 digits!\n";
+			exit(-1);
+		}
 
-        num -= 1000000;
-        arr[num >> (sizeof(uint32_t) + 1)] |= 1 << (UINT_BIT - (num & (UINT_BIT - 1)) - 1);
-    }
+		num -= 1000000;
+		arr[num >> (sizeof(uint32_t) + 1)] |= 1 << (UINT_BIT - (num & (UINT_BIT - 1)) - 1);
+	}
 }
 
-void output(const uint32_t* arr) {
-    for (int i = 0; i < SIZE * UINT_BIT; i++)
-        if (arr[i >> UINT_SHIFT] & 1 << (UINT_BIT- (i & (UINT_BIT - 1)) - 1))
-            std::cout << i + 1000000 << '\n';
+void output(const uint32_t *arr) {
+	for (int i = 0; i < SIZE * UINT_BIT; i++)
+		if (arr[i >> UINT_SHIFT] & 1 << (UINT_BIT - (i & (UINT_BIT - 1)) - 1))
+			std::cout << i + 1000000 << '\n';
 }
