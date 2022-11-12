7269909b869049db4ea7494c2f73041853f88bab
sad <39193849+vyragosa@users.noreply.github.com>
2022-11-12 19:52:48 +0300

    Update main.cpp

--- Practice1_2/main.cpp
+++ Practice1_2/main.cpp
@@ -4,18 +4,19 @@
 #define UINT_SHIFT (sizeof(uint32_t) + 1)
 #define SIZE (8999999 / UINT_BIT + 1)
 
-void sorting(uint32_t *arr);
+int sorting(uint32_t* arr);
 
-void output(const uint32_t *arr);
+void output(const uint32_t* arr);
 
 int main() {
-	uint32_t *arr = new uint32_t[SIZE]{0};
-	sorting(arr);
+	uint32_t* arr = new uint32_t[SIZE]{0};
+	if (sorting(arr) == -1)
+		return -1;
 	output(arr);
 	return 0;
 }
 
-void sorting(uint32_t *arr) {
+int sorting(uint32_t* arr) {
 	uint32_t num, arr_size;
 	std::cout << "Enter size of array: ";
 	std::cin >> arr_size;
@@ -24,15 +25,16 @@ void sorting(uint32_t *arr) {
 		std::cin >> num;
 		if (num > 9999999 || num < 1000000) {
 			std::cerr << "The number must contain only 7 digits!\n";
-			exit(-1);
+			return -1;
 		}
 
-		num %= 1000000;
+		num -= 1000000;
 		arr[num >> UINT_SHIFT] |= 1 << (num & (UINT_BIT - 1));
 	}
+	return 0;
 }
 
-void output(const uint32_t *arr) {
+void output(const uint32_t* arr) {
 	for (uint32_t i = 0; i < SIZE * UINT_BIT; i++)
 		if (arr[i >> UINT_SHIFT] & 1 << (i & (UINT_BIT - 1)))
 			std::cout << i + 1000000 << '\n';
