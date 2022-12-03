f382d52cb993084455f0af6285a700036a440e50
uumlaut <39193849+vyragosa@users.noreply.github.com>
2022-12-03 17:05:44 +0300

    Git Commit

---
+++ Practice8/RLE.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+std::string RLE(const std::string &str) {
+	std::string result;
+	int count = 1;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == str[i + 1]) {
+			count++;
+		} else {
+			result += str[i];
+			if (count > 1) {
+				result += std::to_string(count);
+			}
+				count = 1;
+		}
+	}
+	return result;
+}
+
+int main() {
+	std::string str;
+	std::cin >> str;
+//	WWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
+//	ABCABCABCDDDFFFFFF
+	std::cout << RLE(str);
+	return 0;
+}
